25dcbc8bb880db120b98ac19a11308e72c045498
Catherine Looper <loopercat@gmail.com>
2017-10-30 20:14:49 -0700

    Completed Code Wars Challenge Nice Array

---
+++ nice-array/solution.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function isNice(arr){
+  if (arr.length === 0) {
+    return false;
+  }
+  for(let i = 0; i < arr.length; i++) {
+
+    if (arr.indexOf(arr[i] + 1) === -1 && arr.indexOf(arr[i] - 1) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+isNice();
